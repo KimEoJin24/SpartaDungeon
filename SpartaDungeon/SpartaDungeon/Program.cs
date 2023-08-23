8bef49b947ff4b61d03377917cc9f635cccfa1c9
KimEoJin24 <kamejk24@gmail.com>
2023-08-23 10:33:10 +0900

    Add

--- SpartaDungeon/SpartaDungeon/Program.cs
+++ SpartaDungeon/SpartaDungeon/Program.cs
@@ -1,6 +1,14 @@
-﻿internal class Program
+﻿using System.ComponentModel;
+
+internal class Program
 {
     private static Character player;
+    private static Item 낡은지팡이;
+    private static Item 낡은로브; // 
+    
+    // item을 리스트화
+
+    // 아이템을 더 간략하게 할 수 없을까?
     static void Main(string[] args)
     {
         GameDataSetting();
@@ -9,8 +17,10 @@
 
     static void GameDataSetting()
     {
-        player = new Character("하늘연달", "마법사", 1, 100, 50, 10, 10, 25);
-        // +아이템 정보 세팅
+        player = new Character("하늘연달", "마법사", 1, 100, 50, 5, 20, 5, 10, 25);
+        낡은지팡이 = new Item("낡은 지팡이", 0, 5, 1, 10, 0, 0, "하급마법사들이 사용하던 지팡이");
+        낡은로브 = new Item("낡은 로브",20, 5, 0, 0, 0, 10, "하급마법사들이 입던 로브");
+
     }
 
     static void DisplayGameIntro()
@@ -43,16 +53,22 @@
         Console.WriteLine($"{player.Name}({player.Job}, Lv.{player.Level})\n");
         Console.WriteLine($"체력: {player.Hp}");
         Console.WriteLine($"마나: {player.Mp}");
-        Console.WriteLine($"공격력: {player.Attack}");
-        Console.WriteLine($"방어력: {player.Defence}");
+        Console.WriteLine($"물리공격력: {player.PhysicalAttackPower}");
+        Console.WriteLine($"마법공격력: {player.MagicAttackPower}");
+        Console.WriteLine($"물리방어력: {player.PhysicalDefence}");
+        Console.WriteLine($"마법방어력: {player.PhysicalDefence}");
         Console.WriteLine($"Gold: {player.Gold}G\n");
-        Console.WriteLine("0. 나가기");
+        Console.WriteLine("0. 나가기\n");
+        Console.WriteLine("원하시는 행동을 입력해주세요.");
+        Console.Write(">>");
+        
+
 
         int input = CheckValidInput(0, 0);
         switch (input)
         {
             case 0: 
-                DisplayInventory();
+                DisplayGameIntro();
                 break;
         }
     }
@@ -60,9 +76,27 @@
     static void DisplayInventory()
     {
         Console.Clear();
-        Console.WriteLine("인벤토리 - 장착 관리");
-        Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+        Console.WriteLine("인벤토리");
+        Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
         Console.WriteLine("[아이템 목록]");
+        Console.WriteLine($" -{낡은지팡이.Name}, | 마나 +{낡은지팡이.Mp}, 물리공격력 +{낡은지팡이.PhysicalAttackPower}, 마법공격력 +{낡은지팡이.MagicAttackPower} | {낡은지팡이.Info}");
+        Console.WriteLine($" -{낡은로브.Name},   | 체력 +{낡은로브.Hp}, 마나 +{낡은로브.Mp}, 마법방어력 +{낡은로브.MagicDefence}      | {낡은로브.Info}\n");
+        Console.WriteLine("1. 장착관리");
+        Console.WriteLine("0. 나가기\n");
+        Console.WriteLine("원하시는 행동을 입력해주세요.");
+        Console.WriteLine(">>");
+
+
+        int input = CheckValidInput(0, 1);
+        switch (input)
+        {
+            case 0:
+                DisplayGameIntro();
+                break;
+            case 1:
+                DisplayInventoryEquip();
+                break;
+        }
     }
 
     static int CheckValidInput(int min, int max)
@@ -81,6 +115,30 @@
             Console.WriteLine("잘못된 입력입니다.");
         }
     }
+
+    static void DisplayInventoryEquip()
+    {
+        Console.Clear();
+        Console.WriteLine("인벤토리 - 장착 관리");
+        Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
+        Console.WriteLine("[아이템 목록]");
+        Console.WriteLine($" - 1 {낡은지팡이.Name}, | 마나 +{낡은지팡이.Mp}, 물리공격력 +{낡은지팡이.PhysicalAttackPower}, 마법공격력 +{낡은지팡이.MagicAttackPower} | {낡은지팡이.Info}");
+        Console.WriteLine($" - 2 {낡은로브.Name},   | 체력 +{낡은로브.Hp}, 마나 +{낡은로브.Mp}, 마법방어력 +{낡은로브.MagicDefence}      | {낡은로브.Info}\n");
+        Console.WriteLine("0. 나가기\n");
+        Console.WriteLine("원하시는 행동을 입력해주세요.");
+        Console.WriteLine(">>");
+
+        int input = CheckValidInput(0, 2);
+        switch (input)
+        {
+            case 0:
+                DisplayInventory();
+                break;
+            case 1:
+                
+                break;
+        }
+    }
 }
 
 public class Character
@@ -90,20 +148,50 @@ public class Character
     public int Level { get; }
     public int Hp { get; }
     public int Mp { get; }
-    public int Attack { get; }
-    public int Defence { get; }
+    public int PhysicalAttackPower { get; }
+    public int MagicAttackPower { get; }
+    public int PhysicalDefence { get; }
+    public int MagicDefence { get; }
     public int Gold { get; }
+ 
 
-    public Character(string name, string job, int level, int hp, int mp, int attack, int defence, int gold)
+    public Character(string name, string job, int level, int hp, int mp, int physicalAttackPower, int magicAttackPower, int physicalDefence, int magicDefence, int gold)
     {
         Name = name;
         Job = job;
         Level = level;
         Hp = hp;
         Mp = mp;
-        Attack = attack;
-        Defence = defence;
+        PhysicalAttackPower = physicalAttackPower;
+        MagicAttackPower = magicAttackPower;
+        PhysicalDefence = physicalDefence;
+        MagicDefence = magicDefence;
         Gold = gold;
+    }
+}
+
+public class Item
+{
+    public string Name { get; }
+    public int Hp { get; }
+
+    public int Mp { get; }
+    public int PhysicalAttackPower { get; }
+    public int MagicAttackPower { get; }
+    public int PhysicalDefence { get; }
+    public int MagicDefence { get; }
+    public string Info { get; }
+
+    public Item(string name, int hp, int mp, int physicalAttackPower, int magicAttackPower, int physicalDefence, int magicDefence, string info)
+    {
+        Name = name;
+        Hp = hp;
+        Mp = mp;
+        PhysicalAttackPower = physicalAttackPower;      
+        MagicAttackPower = magicAttackPower;
+        PhysicalDefence = physicalDefence;
+        MagicDefence = magicDefence;
+        Info = info;
     }
 }
 
