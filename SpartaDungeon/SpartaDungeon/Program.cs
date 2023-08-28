777777d48e513d97298ee7e600b16618c9678b3e
KimEoJin24 <kamejk24@gmail.com>
2023-08-28 23:57:12 +0900

    datasetting

--- SpartaDungeon/SpartaDungeon/Program.cs
+++ SpartaDungeon/SpartaDungeon/Program.cs
@@ -6,8 +6,15 @@ internal class Program
     static Item[] inventory;
     static void Main(string[] args)
     {
+        DataSetting(); // 데이터를 먼저 집어 넣어주기.
         GameStartDisplay();
-        player = new Character(level: 01, name: "Chad", atk: 10, def: 5, hp: 100, gold : 1500);
+        
+    }
+
+    static void DataSetting()
+    {
+        player = new Character(level: 1, name: "Chad", job: "전사", atk: 10, def: 5, hp: 100, gold: 1500);
+        inventory = new Item[10];
         inventory[0] = new Item(name: "무쇠갑옷", atk: 0, def: 5, info: "무쇠로 만들어져 튼튼한 갑옷입니다.");
         inventory[1] = new Item(name: "낡은 검", atk: 2, def: 0, info: "쉽게 볼 수 있는 낡은 검입니다.");
     }
@@ -23,7 +30,7 @@ internal class Program
         Console.WriteLine("2. 인벤토리");
         Console.WriteLine();
         Console.WriteLine("원하시는 행동을 입력해주세요.");
-        Console.Write(">>");
+        Console.Write(">> ");
 
         int input = int.Parse(Console.ReadLine());
         if (input == 1)
@@ -47,16 +54,16 @@ internal class Program
         Console.WriteLine("캐릭터의 정보가 표시됩니다.");
         Console.WriteLine();
         Console.WriteLine($"Lv. {player.Level}");
-        Console.WriteLine("Chad ( 전사 )");
-        Console.WriteLine("공격력 : 10");
-        Console.WriteLine("방어력 : 5");
-        Console.WriteLine("체력 : 100");
-        Console.WriteLine("Gold : 1500 G");
+        Console.WriteLine($"{player.Name} ( {player.Job} )");
+        Console.WriteLine($"공격력 : {player.Atk}");
+        Console.WriteLine($"방어력 : {player.Def}");
+        Console.WriteLine($"체력 : {player.Hp}");
+        Console.WriteLine($"Gold : {player.Gold} G");
         Console.WriteLine();
         Console.WriteLine("0. 나가기");
         Console.WriteLine();
         Console.WriteLine("원하시는 행동을 입력해주세요.");
-        Console.Write(">>");
+        Console.Write(">> ");
 
         int input = int.Parse(Console.ReadLine());
         if (input == 0)
@@ -77,14 +84,27 @@ internal class Program
         Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
         Console.WriteLine();
         Console.WriteLine("[아이템 목록]");
-        Console.WriteLine("- [E]무쇠갑옷      | 방어력 +5 | 무쇠로 만들어져 튼튼한 갑옷입니다.");
-        Console.WriteLine("- 낡은 검         | 공격력 +2 | 쉽게 볼 수 있는 낡은 검 입니다.");
+        for(int i=0; i<inventory.Length; i++)
+        {
+            if (inventory[i] == null)
+            {
+                break;
+            }
+            if (inventory[i].Atk == 0)
+            {
+                Console.WriteLine($"- {inventory[i].Name}    | 방어력 +{inventory[i].Def} | {inventory[i].Info}");
+            }
+            else if (inventory[i].Def == 0)
+            {
+                Console.WriteLine($"- {inventory[i].Name}    | 공격력 +{inventory[i].Atk} | {inventory[i].Info}");
+            }
+        }
         Console.WriteLine();
         Console.WriteLine("1. 장착 관리");
         Console.WriteLine("0. 나가기");
         Console.WriteLine();
         Console.WriteLine("원하시는 행동을 입력해주세요.");
-        Console.Write(">>");
+        Console.Write(">> ");
 
         int input = int.Parse(Console.ReadLine());
         if (input == 1)
@@ -109,13 +129,27 @@ internal class Program
         Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
         Console.WriteLine();
         Console.WriteLine("[아이템 목록]");
-        Console.WriteLine("- [E]무쇠갑옷      | 방어력 +5 | 무쇠로 만들어져 튼튼한 갑옷입니다.");
-        Console.WriteLine("- 낡은 검         | 공격력 +2 | 쉽게 볼 수 있는 낡은 검 입니다.");
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory[i] == null)
+            {
+                break;
+            }
+            Console.Write(" - " + (i + 1));
+            if (inventory[i].Atk == 0)
+            {
+                Console.WriteLine($" {inventory[i].Name}    | 방어력 +{inventory[i].Def} | {inventory[i].Info}");
+            }
+            else if (inventory[i].Def == 0)
+            {
+                Console.WriteLine($" {inventory[i].Name}    | 공격력 +{inventory[i].Atk} | {inventory[i].Info}");
+            }
+        }
         Console.WriteLine();
         Console.WriteLine("0. 나가기");
         Console.WriteLine();
         Console.WriteLine("원하시는 행동을 입력해주세요.");
-        Console.Write(">>");
+        Console.Write(">> ");
 
         int input = int.Parse(Console.ReadLine());
         if (input == 0)
@@ -133,15 +167,17 @@ class Character
 {
     public int Level;
     public string Name;
+    public string Job;
     public int Atk;
     public int Def;
     public int Hp;
     public int Gold;
 
-    public Character(int level, string name, int atk, int def, int hp, int gold)
+    public Character(int level, string name, string job, int atk, int def, int hp, int gold)
     {
         Level = level;
         Name = name;
+        Job = job;
         Atk = atk;
         Def = def;
         Hp = hp;    
